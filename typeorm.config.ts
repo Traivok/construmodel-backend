5c6accc520dafc65f163e63a67c4d037f7e04b4c
José Ricardo Alves Figueirôa <jraf@cin.ufpe.br>
2022-11-09 10:35:41 -0300

    verbose

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,15 +1,16 @@
-import getConfig      from './config';
-import { DataSource } from 'typeorm';
-import { Logger }     from '@nestjs/common';
+import getConfig                         from './config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { Logger }                        from '@nestjs/common';
 
 const config = getConfig();
 
 const logger = new Logger('TypeormConfig');
 logger.log(`Connecting to database ${ config.DB_NAME } as ${ config.DB_USER }`);
 
-export default new DataSource({
+
+const options: DataSourceOptions = {
   type:          'postgres',
-  host:          config.DB_HOST ?? 'localhost',
+  host:          config.DB_HOST,
   database:      config.DB_NAME,
   username:      config.DB_USER,
   password:      config.DB_PASSWORD ?? null,
@@ -18,4 +19,8 @@ export default new DataSource({
   logging:       config.DB_LOG ?? false,
   entities:      [ __dirname + '/src/**/*{.entity,.view.entity}{.ts,.ts}' ],
   migrations:    [ __dirname + '/migrations/**/*{.ts,.js}' ],
-});
+};
+
+console.log({options});
+
+export default new DataSource(options);
